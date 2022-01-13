3464f4431e6885ae9d9bab1b080cc102b5f53929
JeremyDufeux <jeremy.dufeux@gmail.com>
2022-01-13 19:11:36 +0100

    Initial commit

---
+++ app/src/main/java/com/jeremydufeux/u_convert/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.jeremydufeux.u_convert
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.jeremydufeux.u_convert.ui.theme.UConvertTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UConvertTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    UConvertTheme {
+        Greeting("Android")
+    }
+}
